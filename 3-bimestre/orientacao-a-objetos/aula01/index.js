34bb7821fe0f36e69ded96502233b0108a40c35b
Larissa Ramos <maria.sousa6250@aluno.ce.gov.br>
2026-08-07 19:54:25 +0000

    feito

---
+++ 3-bimestre/orientacao-a-objetos/aula01/index.js
@@ -0,0 +1,14 @@
+const filme = {
+    titulo: "Interestelar",
+    diretor:"Christopher Nolan",
+    ano:2014,
+    apresentar() {
+   
+        return `Filme: Interestelar`
+
+
+    }
+
+    }
+    
+module.exports = filme
